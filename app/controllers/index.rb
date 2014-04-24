bd6f96326e872b9a9b4ce4541cae559d9ce2bc66
raorao <savinirs@gmail.com>
2014-04-24 16:55:24 -0700

    adds routes
    only /, get /posts, /post/:id/vote, are operational. all others are WIP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,30 @@
 get '/' do
+  redirect '/posts'
+end
+
+get '/posts' do
+  @posts = Post.all
   erb :index
+end
+
+get '/post/:id/vote' do
+  post = Post.find(params[:id])
+  post.votes.create(value: 1)
+  redirect "/posts"
+end
+
+delete '/post/:id' do
+  post = Post.find(params[:id])
+  post.destroy
+  redirect "/posts"
+end
+
+post '/posts' do
+  Post.create(params)
+  redirect '/posts'
+end
+
+get '/post/:id' do
+  @post = Post.find(params[:id])
+  erb :post
 end
